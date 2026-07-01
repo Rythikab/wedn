82ce3bce40d19df14c296c53ccdc68de56149d62
Rythika <144686263+Rythikab@users.noreply.github.com>
2026-07-01 07:58:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    alert("Welcome to Smart Gym & Fitness Management System!");
+});
+
+
+handler: function(response) {
+    alert("payment successful");
+    window.location.href = "success.html";
